6a95037fd9b8733981fd2caa6c54aaa7b72149ef
Ilse Noorlander <ilsenoorlander@gmail.com>
2024-11-19 13:38:33 +0100

    make small changes to index.js

--- src/index.js
+++ src/index.js
@@ -16,24 +16,14 @@ const app = express();
 Sentry.init({
   dsn: "https://fb4e58b4649549012dda1dc045059144@o4508177459118080.ingest.de.sentry.io/4508324631150672",
   integrations: [
-    // enable HTTP calls tracing
     new Sentry.Integrations.Http({ tracing: true }),
-    // enable Express.js middleware tracing
     new Sentry.Integrations.Express({ app }),
-    // Automatically instrument Node.js libraries and frameworks
     ...Sentry.autoDiscoverNodePerformanceMonitoringIntegrations(),
   ],
-
-  // Set tracesSampleRate to 1.0 to capture 100%
-  // of transactions for performance monitoring.
-  // We recommend adjusting this value in production
   tracesSampleRate: 1.0,
 });
 
-// RequestHandler creates a separate execution context, so that all
-// transactions/spans/breadcrumbs are isolated across requests
 app.use(Sentry.Handlers.requestHandler());
-// TracingHandler creates a trace for every incoming request
 app.use(Sentry.Handlers.tracingHandler());
 
 app.use(express.json());
